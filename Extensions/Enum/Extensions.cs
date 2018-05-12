eee14f0c034bdc9fad3c7c6ce2b76295698da98a
RSado <r.sadocchi@outlook.it>
2018-05-12 16:46:22 +0200

    bug fixed

--- Extensions/Enum/Extensions.cs
+++ Extensions/Enum/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using Extensions.Attributes;
 using System;
+using System.Reflection;
 
 namespace Extensions.Enum
 {
@@ -8,19 +9,19 @@ namespace Extensions.Enum
         public static string GetStringValue<T>(this T source) where T : struct, IConvertible
         {
             if (!typeof(T).IsEnum) return null;
-            Type attr = typeof(T);
-            StringValue value = (StringValue)Attribute.GetCustomAttribute(attr, typeof(StringValue));
+            MemberInfo info = typeof(T).GetField(System.Enum.GetName(typeof(T), source));
+            StringValue value = (StringValue)Attribute.GetCustomAttribute(info, typeof(StringValue));
             if (value == null) return null;
             return value.Value;
         }
 
-        public static TValue GetComplexValue<T, TValue>(this T sourcee)
+        public static TValue GetComplexValue<T, TValue>(this T source)
             where T : struct, IConvertible
             where TValue : Type
         {
             if (!typeof(T).IsEnum) return null;
-            Type attr = typeof(TValue);
-            ComplexValue value = (ComplexValue)Attribute.GetCustomAttribute(attr, typeof(ComplexValue));
+            MemberInfo info = typeof(T).GetField(System.Enum.GetName(typeof(T), source));
+            ComplexValue value = (ComplexValue)Attribute.GetCustomAttribute(info, typeof(ComplexValue));
             if (value == null) return null;
             if (value.ValueType != null && value.ValueType != typeof(TValue))
                 throw new InvalidCastException($"Value is type of '{value.ValueType.GetType()}', cannot be casted in {typeof(TValue)}");
